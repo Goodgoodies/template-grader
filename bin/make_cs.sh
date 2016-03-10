f5af92549637084df87ac5fe93f841ef791f8b15
Spencer Pollock <srepollock@gmail.com>
2016-03-10 12:08:06 -0800

    :sleepy: Silly errors. Just doing some testing that the files work when calling from C:/Users/Spencer/.templater/bin

---
+++ bin/make_cs.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#make_cs - A Script to make a C# class document
+
+## Functions
+
+help() {
+	(echo "Create a basic Java tempalte"
+	echo "Must have a name, package and an output"
+	echo "Usage: [Class Name] [Package name] > [output file name].java") 1>&2
+}
+# Check
+
+if [ "${1}" == "--help" ]; then 
+	help
+	exit 0
+fi
+
+if [ $# -gt 3 ]; then
+	help
+	exit 1;
+fi
+
+CLASSNAME=${1}
+PACKAGE=${2}
+TIME=$(date +"%x %r %Z")
+
+cat << _EOF_
+package $PACKAGE;
+
+/** 
+  * $CLASSNAME class
+  *
+  * Created by $USER $TIME
+  */
+
+public $CLASSNAME {
+	public $CLASSNAME() {
+		
+	}
+	public static void main(String[] args){
+		// System.out.println("$CLASSNAME has been created.");
+	}
+}
+_EOF_
