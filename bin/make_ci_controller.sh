efe9505eee7c84c77e3d5e8ccfc12b79ccfa04b9
Spencer Pollock <srepollock@gmail.com>
2016-03-10 12:20:30 -0800

    :smiley: Fixed user name for output file. Added CodeIgniter CI_Controller template

--- bin/make_ci_controller.sh
+++ bin/make_ci_controller.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#make_ci_model - A Script to make a CodeIgniter Model class document
+#make_ci_controller - A Script to make a CodeIgniter Controller class document
 
 ## Functions
 
 help() {
-	(echo "Create a CodeIgniter Model tempalte"
-	echo "Must have a name, package and an output"
-	echo "Usage: [Model Name] > [Model Name].php") 1>&2
+	(echo "Create a CodeIgniter Controller tempalte"
+	echo "Must have a name and an output"
+	echo "Usage: [Controller Name] > [Controller Name].php") 1>&2
 }
 # Check
 
@@ -21,6 +21,7 @@ if [ $# -gt 2 ]; then
 	exit 1;
 fi
 
+USER=$(id -u -n) 
 CLASSNAME=${1}
 TIME=$(date +"%x %r %Z")
 
@@ -28,12 +29,12 @@ cat << _EOF_
 <?php
 
 /** 
-  * $CLASSNAME Model class
+  * $CLASSNAME Controller class
   *
   * Created by $USER $TIME
   */
 
-class $CLASSNAME extends CI_Model {
+class $CLASSNAME extends CI_Controller {
     function __construct()
     {
         // Call the Model constructor
