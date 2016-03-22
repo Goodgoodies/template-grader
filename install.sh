fe78b13162fd5aa2e33cf8d572e83c0bd1bc815e
Spencer Pollock <srepollock@gmail.com>
2016-03-22 16:17:10 -0700

    :tired_face: Minor fixes

--- install.sh
+++ install.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
-comment="# Template Grader Update Export"
+comment="# Template Grader Export"
 output="export PATH=\"\$HOME/.templater/update:\$PATH\""
+makedir="export PATH=\"\$HOME/.templater/bin:\$PATH\""
 if [ -f ~/.zshrc ]; then
     # if ~/.zshrc is found
-    if grep -Fxq "$comment" ~/.zshrc && grep -Fxq "$output" ~/.zshrc
+    if grep -Fxq "$comment" ~/.zshrc && grep -Fxq "$output" ~/.zshrc && grep -Fxq "$makedir" ~/.zshrc
     then
         # Lines found
         echo "Already added to ~/.zshrc";
     else
         echo $comment >> ~/.zshrc
         echo $output >> ~/.zshrc
+        echo $makedir >> ~/.zshrc
     fi
 fi
-if grep -Fxq "$comment" ~/.bashrc && grep -Fxq "$output" ~/.bashrc
+if grep -Fxq "$comment" ~/.bashrc && grep -Fxq "$output" ~/.bashrc && grep -Fxq "$makedir" ~/.bashrc
     then
         # Lines found
         echo "Already added to ~/.bashrc";
     else
         echo $comment >> ~/.bashrc
         echo $output >> ~/.bashrc
+        echo $makedir >> ~/.bashrc
     fi
 bash -c "chmod a+x ~/.templater/update/template_grader_update.sh"
+bash -c "chmod a+x ~/.templater/bin/*.sh"
