10d05263c445d63859e684978c9427d167c33d8d
Spencer Pollock <srepollock@gmail.com>
2016-03-22 15:51:25 -0700

    :skull: Fixed update file mispelling

--- update/template_grader_update.sh
+++ update/template_grader_update.sh
@@ -2,6 +2,6 @@
 # Clear previous alias
 git config --gloabl --unset alias.template_update
 # Setup an alias for the update
-git config --global alias.tempalte_update '!git remote update -p; git merge -ff-only @{u}'
+git config --global alias.template_update '!git remote update -p; git merge -ff-only @{u}'
 # Echo into a new bash
 bash -c "cd ~/.templater && git checkout master && git template_update && exit"
