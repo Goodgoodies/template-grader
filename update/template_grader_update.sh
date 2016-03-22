aabfe00e632cc662cbec98116d36e7b555485163
Spencer Pollock <srepollock@users.noreply.github.com>
2016-03-22 16:07:05 -0700

    Update template_grader_update.sh

--- update/template_grader_update.sh
+++ update/template_grader_update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Setup an alias for the update
-git config --global alias.templateupdate '!git remote update -p; git merge -ff-only @{u}'
+git config --global alias.templateupdate '!git remote update -p; git merge --ff-only @{u}'
 # Echo into a new bash
 bash -c "cd ~/.templater && git checkout master && git templateupdate && exit"
